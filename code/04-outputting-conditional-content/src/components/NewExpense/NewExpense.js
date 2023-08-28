edce95cf4fb54ad9c29ef581dd5c80bf140ba3e4
Kevin Agus <kevin.agus@alten.it>
2023-08-28 17:46:30 +0200

    fix: solution section 5 ex 4

--- code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
+++ code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [newExpenseFlag, setNewExpenseFlag] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+  const newExpenseHandler = () => {
+    setNewExpenseFlag(true);
+  };
+
+  const cancelClickedHandler = () => {
+    setNewExpenseFlag(false);
+  };
+
+  let newExpenseContent = (
+    <button onClick={newExpenseHandler} type="button">
+      Add New Expense
+    </button>
   );
+
+  if (newExpenseFlag) {
+    newExpenseContent = (
+      <ExpenseForm
+        onSubmitOrCancelClicked={cancelClickedHandler}
+        onSaveExpenseData={saveExpenseDataHandler}
+      />
+    );
+  }
+
+  return <div className="new-expense">{newExpenseContent}</div>;
 };
 
 export default NewExpense;
