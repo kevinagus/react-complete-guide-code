1884358435d81b6ed7b83fc4b64fd9c91483ca2a
Kevin Agus <kevin.agus@alten.it>
2023-09-28 11:53:13 +0200

    fix: solution section 18 ex 10

--- code/10-working-with-multiple-slices/src/App.js
+++ code/10-working-with-multiple-slices/src/App.js
@@ -1,15 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import { useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </Fragment>
   );
